153d67206d241f01dc9b1c0b2b7e605a0358d717
PaMLaM876 <gamesking529@gmail.com>
2025-12-06 22:08:24 +0530

    Pushing Project

---
+++ src/main/java/org/pam/patri/PatriApplication.java
@@ -0,0 +1,13 @@
+package org.pam.patri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatriApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatriApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/pam/patri/PatriApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pam.patri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatriApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
