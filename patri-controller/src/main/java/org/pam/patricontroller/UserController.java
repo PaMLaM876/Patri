10f77b652c73c5bab3e422242aab90ee00a09a81
PaMLaM876 <gamesking529@gmail.com>
2025-12-07 21:10:01 +0530

    Pushing Project

---
+++ patri-controller/src/main/java/org/pam/patricontroller/UserController.java
@@ -0,0 +1,4 @@
+package src.main.java.org.pam.patricontroller;
+
+public class UserController {
+}
--- patri-restfulservice/src/main/java/org/pam/patrirestfulservice/PatriApplication.java
+++ patri-restfulservice/src/main/java/org/pam/patrirestfulservice/PatriApplication.java
@@ -1,13 +1,9 @@
 package org.pam.patrirestfulservice;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class PatriApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(PatriRestfulserviceApplication.class, args);
-    }
-
+public static void main(String[] args) {
+    SpringApplication.run(PatriApplication.class, args);
+}
 }
